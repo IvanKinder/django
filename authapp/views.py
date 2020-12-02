4abe99d6cb024598e01cd98399677a6bc284e2ca
Ivan Kinder <ivankinder11@gmail.com>
2020-12-02 09:27:48 +0300

    че-то делается

--- authapp/views.py
+++ authapp/views.py
@@ -1,8 +1,31 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
+from authapp.forms import ShopUserLoginForm
+
+
 def login(request):
-    pass
+    login_form = ShopUserLoginForm(data=request.POST)
+    if request.method == 'POST' and login_form.is_valid():
+        username = request.POST.get('username')
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main'))
+    content = {'login_form': login_form}
+    return render(request, 'authapp/login.html', content)
 
 def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('main'))
+
+def register(request):
+    pass
+
+def edit(request):
     pass
