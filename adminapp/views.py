f9ee98f0398e3f3b45a5238ce4c507d7600d7181
Ivan Kinder <ivankinder11@gmail.com>
2020-12-11 13:41:21 +0300

    лесон 7

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,14 +1,30 @@
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
+from authapp.forms import ShopUserRegisterForm
 from authapp.models import ShopUser
 
 
 # Users
 
 def user_create(request):
-    pass
+    if request.method == 'POST':
+        user_form = ShopUserRegisterForm(request.POST, request.FILES)
+        if user_form.is_valid():
+            user_form.save()
+            return HttpResponseRedirect(reverse('admin:users'))
+    else:
+        user_form = ShopUserRegisterForm()
+
+    content = {
+        'update_form': user_form
+    }
+    return render(request, 'adminapp/user_update.html', content)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def users(request):
     users_list = ShopUser.objects.all().order_by('is_active')
     content = {
