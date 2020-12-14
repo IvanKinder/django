9252aab1bcc7a671ac89dd11e2fd26875e1703e6
Ivan Kinder <ivankinder11@gmail.com>
2020-12-14 11:57:24 +0300

    гачало лессон7

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,13 +1,16 @@
 from django.contrib.auth.decorators import user_passes_test
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 
+from adminapp.forms import ShopUserAdminEditForm
 from authapp.forms import ShopUserRegisterForm
 from authapp.models import ShopUser
 
 
 # Users
+from mainapp.models import ProductCategory
+
 
 def user_create(request):
     if request.method == 'POST':
@@ -26,19 +29,47 @@ def user_create(request):
 
 @user_passes_test(lambda u: u.is_superuser)
 def users(request):
+    title = 'Админка'
     users_list = ShopUser.objects.all().order_by('is_active')
     content = {
-        'objects': users_list
+        'objects': users_list,
+        'title': title
     }
     return render(request, 'adminapp/users.html', content)
 
 
 def user_update(request, pk):
-    pass
+    edit_user = get_object_or_404(ShopUser, pk=pk)
+    if request.method == 'POST':
+        edit_form = ShopUserAdminEditForm(request.POST, request.FILES, instance=edit_user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('adminapp:user_update', args=[edit_user.pk]))
+    else:
+        edit_form = ShopUserAdminEditForm(instance=edit_user)
+
+    content = {
+        'update_form': edit_form
+    }
+
+    return render(request, 'adminapp/user_update.html', content)
 
 
 def user_delete(request, pk):
-    pass
+    user_item = get_object_or_404(ShopUser, pk=pk)
+
+    if request.method == 'POST':
+        if user_item.is_active:
+            user_item.is_active = False
+        else:
+            user_item.is_active = True
+        user_item.save()
+        return HttpResponseRedirect(reverse('admin:users'))
+
+    content = {
+        'user_to_delete': user_item
+    }
+    return render(request, 'adminapp/user_delete.html', content)
 
 # Categories
 
@@ -47,7 +78,12 @@ def category_create(request):
 
 
 def categories(request):
-    pass
+    categories_list = ProductCategory.objects.all().order_by('is_active')
+    content = {
+        'objects': categories_list
+    }
+
+    return render(request, '', content)
 
 
 def category_update(request, pk):
--- mainapp/models.py
+++ mainapp/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class ProductCategory(models.Model):
     name = models.CharField(max_length=64, unique=True, verbose_name='Имя')
     discription = models.TextField(verbose_name='Описание')
+    is_active = models.BooleanField(default=True, verbose_name='активна')
 
     def __str__(self):
         return self.name
@@ -16,6 +17,7 @@ class Product(models.Model):
     discription = models.TextField(verbose_name='Описание', blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0, verbose_name='Цена')
     quantity = models.PositiveSmallIntegerField(default=0, verbose_name='Количество на складе')
+    is_active = models.BooleanField(default=True, verbose_name='активна')
 
     def __str__(self):
         return f'{self.name} ({self.category.name})'
