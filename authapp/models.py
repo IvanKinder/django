fa8ae5d167727c2d12cbef90e4b6e65db221a7cc
Ivan Kinder <ivankinder11@gmail.com>
2020-12-16 13:27:08 +0300

    l8

--- authapp/models.py
+++ authapp/models.py
@@ -5,3 +5,4 @@ from django.db import models
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatars', blank=True, verbose_name='аватар')
     age = models.PositiveSmallIntegerField(verbose_name='возраст')
+
